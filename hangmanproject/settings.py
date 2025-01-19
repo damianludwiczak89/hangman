981a0e348a99080fd56a20f1b43effe01df5b679
damianludwiczak89 <damian_konin@tlen.pl>
2025-01-19 21:45:18 +0100

    secret key

--- hangmanproject/settings.py
+++ hangmanproject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-t5_&0h41&q)=jytghs2ajpx^hpyb3$e0p=b2dkh)%9x2v5$3(v"
+SECRET_KEY = os.environ['SECRET-KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
